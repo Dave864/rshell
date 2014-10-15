acb84713632f83f2f77e695682f9278ea96d3872
Dave864 <amacp001@gmail.com>
2014-10-15 00:01:44 -0700

    began attempting to parse the command

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <string.h>
 
+char ** parse(string & command)
+{
+	int sz = 0;
+	for(int i = 0; i < command.size(); i++)
+	{
+		sz++;
+		if(isspace(command[i]))
+		{
+			
+		}
+	}
+}
+
 int main()
 {
 	std::cout << "$ ";
 	std::string command;
 	getline(std::cin, command);
-	std::cout << command << std::endl;
 	return 0;
 }
