882a5ad4347157d5d2731a6a9a5d0aa9b34b259a
Dave864 <amacp001@gmail.com>
2014-10-16 16:52:42 -0700

    Began working on comment check. I plan on checking for comments before checking for connectors since comments are to be ignored anyway

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,39 @@
 #include <string>
 #include <string.h>
 
+void parse(std::string & command, const char * connector)
+{
+	return;
+}
+
 int main()
 {
 	//recieves the input from the user
 	std::cout << "$ ";
-	std::string command;
-	getline(std::cin, command);
+	std::string input;
+	getline(std::cin, input);
+	//Checks to if there are any comments and ignores them if they exist
+	char * command; 
 	//determines if there are any connectors and sets them as the token
 	//if one exists
-	int in_of_connector;
-	if(command.find(';') > -1)
+	/*const char * sem_Col = ";";
+	const char * or_Op = "||";
+	const char * and_Op = "&&";
+	if(command.find(';') != std::string::npos)
 	{
 		//set token as ';' and parse through command
+		parse(command, sem_Col);
 	}
-	else if(command.find("||") > -1)
+	else if(command.find("||") != -std::string::npos)
 	{
 		//set token as "||" and parse through command
+		parse(command, or_Op);
 	}
-	else if(command.find("&&") > -1)
+	else if(command.find("&&") != std::string::npos) 
 	{
 		//set token as "&&" and parse through command
-	}
+		parse(command, and_Op);
+	}*/
 	//run execvp on command
 	return 0;
 }
