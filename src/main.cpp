582c1dca0e3e5c8fd1f6dc456672ae38baf55516
Dave864 <amacp001@gmail.com>
2014-10-16 22:53:16 -0700

    The program is now able to distinguish which type of connector is used

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,31 @@
 #include <string>
 #include <string.h>
 
-void parse(char *& command, const char *& connector)
+void Parse(char *& word, const char *& connector)
 {
+	char * command = strtok(word, connector);
+	std::cout << command << std::endl;
 	return;
+}
+
+//Tells if c is in word
+bool FindC(char *& word, const char * c)
+{
+	for(unsigned int i = 0; i < sizeof(word-sizeof(c)+1); i++)
+	{
+		if(c[0] == word[i])
+		{
+			for(unsigned int j = 1; j < sizeof(word); j++)
+			{
+				if(c[j] != word[i+j])
+				{
+					break;
+				}
+			}
+			return true;
+		}
+	}
+	return false;
 }
 
 int main()
@@ -23,30 +45,33 @@ int main()
 	char * c_str_in = new char [input.size()+1];
 	strcpy(c_str_in, input.c_str()); 
 	char * command = strtok(c_str_in, "#");
-	std::cout << command << std::endl;
-	delete[] c_str_in;
 
 	//determines if there are any connectors and sets them as the token
 	//if one exists
+	/*
 	const char * sem_Col = ";";
 	const char * or_Op = "||";
 	const char * and_Op = "&&";
-	std::string tmp_com(command);
-	if(tmp_com.find(';') != std::string::npos)
+	*/
+	if(FindC(command, ";"))
 	{
 		//set token as ';' and parse through command
-		parse(command, sem_Col);
+		//Parse(command, sem_Col);
+		std::cout << "connector is a ;\n";
 	}
-	else if(tmp_com.find("||") != -std::string::npos)
+	else if(FindC(command, "||"))
 	{
 		//set token as "||" and parse through command
-		parse(command, or_Op);
+		//Parse(command, or_Op);
+		std::cout << "connector is a ||\n";
 	}
-	else if(tmp_com.find("&&") != std::string::npos) 
+	else if(FindC(command, "&&"))
 	{
 		//set token as "&&" and parse through command
-		parse(command, and_Op);
+		//Parse(command, and_Op);
+		std::cout << "connector is a &&\n";
 	}
 	//run execvp on command
+	delete[] c_str_in;
 	return 0;
 }
