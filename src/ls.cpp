2adc46a1a29f216cc1ba4285cf1a80cb5a2cf6ce
amacp001 <amacp001@ucr.edu>
2014-10-27 15:53:44 -0700

    began working on checking for the flags -a, -l, and -R

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
-int main(int argc, char ** argv)
+//checks argc for any flags and sets the appropriate index in flags if so
+void checkFlags(bool* flags, int argc, char** argv)
 {
+	return;
+}
+
+int main(int argc, char** argv)
+{
+	//indicates if a flag is set
+	//0 -a
+	//1 -1
+	//2 -R
+	bool flags[3] = {false, false, false};
+	checkFlags(flags, argc, argv);
 	return 0;
 }
