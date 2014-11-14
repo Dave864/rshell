4a67a65e2343b3064a9c43650fda73de61429244
amacp001 <amacp001@ucr.edu>
2014-11-13 22:22:19 -0800

    Began work on the basics of command parsing. Able to ignore comments. Able to stop when exit is entered. Able to determine the connector used. Working on creating char* arrays from input for use by 	execvp.

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <string.h>
 #include <stdio.h>
 #include <errno.h>
 using namespace std;
@@ -19,7 +20,7 @@ void DelComment(string& input)
 	input = input.substr(0, input.find(COMMENT));
 }
 
-//Return true if exit was called
+//returns true if exit was called
 bool ExitFound(string input)
 {
 	if(input.find("exit") == string::npos)
@@ -29,19 +30,71 @@ bool ExitFound(string input)
 	return true;
 }
 
+//creates a char** object from a char* for use by execvp
+void GetCom(char** command, const char* input)
+{
+	char tmp[BUFSIZ];
+	strcpy(tmp, input);
+	char* elmt = strtok(tmp, " \t\n\v\f\r");
+	while(elmt != NULL)
+	{
+		//Insert(command, elmt);
+		elmt = strtok(NULL, " \t\n\v\f\r");
+	}
+}
+
+//sees which connector was invoked and executes the input using that connector
+void RunWCon(string input)
+{
+	if(input.find(C_NEXT) != string::npos)
+	{
+		if(input.find(C_NEXT) == 0)
+		{
+			cerr << "error: connector as first argument\n";
+			return;
+		}
+		//parse input and run each command
+	}
+	else if(input.find(C_AND) != string::npos)
+	{
+		if(input.find(C_AND) == 0)
+		{
+			cerr << "error: connector as first argument\n";
+			return;
+		}
+		//parse input and run command if previous also ran
+	}
+	else if(input.find(C_OR) != string::npos)
+	{
+		if(input.find(C_OR) == 0)
+		{
+			cerr << "error: connector as first argument\n";
+			return;
+		}
+		//parse input and run command if previous failed
+	}
+	else
+	{
+		char* command[1] = {NULL};
+		GetCom(command, input.c_str());
+	}
+}
+
 int main()
 {
 	//declare all variables to be used in the rshell loop here
 	string input;
 	while(1)
 	{
+		cout << "$ ";
+		cin >> ws;
 		getline(cin, input);
 		DelComment(input);
 		if(ExitFound(input))
 		{
 			return 0;
 		}
-		cout << input << endl;
+		RunWCon(input);
 	}
 	return 0;
 }
